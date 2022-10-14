524f73f36b685060ac4466cc061741a0ba5f4b7e
Luis Rodrigo Montúfar Pérez <A01273078@itesm.mx>
2022-10-14 07:12:54 -0500

    Backend SetUp
    Creation of endpoints and Waterline models

---
+++ app/api/controllers/car/read.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Read',
+
+
+  description: 'Read car.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
