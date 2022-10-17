4b72c3ea714acd19df1947b4ed111fc82ae38e31
Luis Rodrigo Montúfar Pérez <A01273078@itesm.mx>
2022-10-17 11:30:52 -0500

    User Model SetUp Co-authored-by: Adrian

--- app/api/models/user.js
+++ app/api/models/user.js
@@ -1,6 +1,47 @@
 module.exports ={
     attributes: {
-        
+        id: {
+            type: 'number',
+            required: true,
+            autoIncrement: true
+        },			
+			
+        full_name:{
+            type: 'string', 
+            required: true,
 
+
+        },		
+
+        mail:{
+            type: 'string', 
+            required: true,
+            
+        },		
+
+        phone:{
+            type: 'string', 
+            required: true,
+
+        },			
+
+        password:{
+            type: 'string', 
+            required: true,
+
+        },			
+
+        id_office:{
+            type: 'number',
+            required: false,
+
+        },	
+
+        id_role:{
+            type: 'number',
+            required: false,
+
+        }	
+			
     }
 }
