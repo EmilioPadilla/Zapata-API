dce6103a4fe98a45519a75050b751a7f6e1b1c5f
EmilioPadilla <padillam_@hotmail.com>
2022-10-19 12:49:42 -0500

    delete all front end and unnecessary files

--- config/datastores.js
+++ config/datastores.js
@@ -46,8 +46,8 @@ module.exports.datastores = {
      *                                                                          *
      ***************************************************************************/
     adapter: "sails-postgresql",
-    // url: "postgresql://postgres:diuASDasdasdjnjSA@54.208.219.250:5432/zapata_database", QA environment, uncomment when testing publishing to production else DON'T
+    // url: "postgresql://postgres:diuASDasdasdjnjSA@54.208.219.250:5432/zapata_database" // QA environment: uncomment when testing publishing in production else DON'T
     // DEVELOP IN LOCAL WITH BELOW URL
-    // url: "postgresql://user:password@localhost/db_name"
+    url: "postgresql://Emirock:@localhost/zapata_database",
   },
 };
