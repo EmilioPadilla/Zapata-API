4da121336ad78d3b5a8920816aed3cd3db272463
EmilioPadilla <padillam_@hotmail.com>
2022-10-20 15:40:40 -0500

    Change routes for traditional controllers

---
+++ api/controllers/gps/CreateDeviceController.js
@@ -0,0 +1,22 @@
+const Device = require('../../models/Device');
+
+module.exports = {
+  create: function (req, res) {
+    sails.log.debug(req.allParams());
+    sails.log.debug(req.allParams());
+    Device.create(req.allParams())
+      .then((dispositivos) => {
+        return res.send({
+          success: true,
+          message: 'Record created successfully',
+        });
+      })
+      .catch((err) => {
+        sails.log.debug(err);
+        return res.send({
+          success: false,
+          message: 'Unable to create record',
+        });
+      });
+  }
+};
--- api/controllers/gps/GetDeviceController.js
+++ api/controllers/gps/GetDeviceController.js
@@ -33,22 +33,4 @@ module.exports = {
         });
       });
   },
-
-  create: function (req, res) {
-    sails.log.debug(req.allParams());
-    Device.create(req.allParams())
-      .then((Devices) => {
-        return res.send({
-          success: true,
-          message: 'Record created successfully',
-        });
-      })
-      .catch((err) => {
-        sails.log.debug(err);
-        return res.send({
-          success: false,
-          message: 'Unable to create record',
-        });
-      });
-  },
 };
