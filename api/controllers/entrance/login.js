4a905d199cbe0471f9cf945c3192f785905a9997
EmilioPadilla <padillam_@hotmail.com>
2022-10-20 14:50:12 -0500

    Set Device model and controller to receive GPS values

--- api/controllers/entrance/login.js
+++ api/controllers/entrance/login.js
@@ -1,54 +1,46 @@
+const User = require('../../models/User');
+
 module.exports = {
-
-
   friendlyName: 'Login',
-
 
   description: 'Log in using the provided email and password combination.',
 
-
-  extendedDescription:
-`This action attempts to look up the user record in the database with the
+  extendedDescription: `This action attempts to look up the user record in the database with the
 specified email address.  Then, if such a user exists, it uses
 bcrypt to compare the hashed password from the database with the provided
 password attempt.`,
 
-
   inputs: {
-
     emailAddress: {
       description: 'The email to try in this attempt, e.g. "irl@example.com".',
       type: 'string',
-      required: true
+      required: true,
     },
 
     password: {
-      description: 'The unencrypted password to try in this attempt, e.g. "passwordlol".',
+      description:
+        'The unencrypted password to try in this attempt, e.g. "passwordlol".',
       type: 'string',
-      required: true
-    }
-
+      required: true,
+    },
   },
 
-
   exits: {
-
     success: {
       description: 'The requesting user agent has been successfully logged in.',
-      extendedDescription:
-`Under the covers, this stores the id of the logged-in user in the session
+      extendedDescription: `Under the covers, this stores the id of the logged-in user in the session
 as the \`userId\` key.  The next time this user agent sends a request, assuming
 it includes a cookie (like a web browser), Sails will automatically make this
 user id available as req.session.userId in the corresponding action.  (Also note
 that, thanks to the included "custom" hook, when a relevant request is received
 from a logged-in user, that user's entire record from the database will be fetched
-and exposed as \`req.me\`.)`
+and exposed as \`req.me\`.)`,
     },
 
     badCombo: {
       description: `The provided email and password combination does not
       match any user in the database.`,
-      responseType: 'unauthorized'
+      responseType: 'unauthorized',
       // ^This uses the custom `unauthorized` response located in `api/responses/unauthorized.js`.
       // To customize the generic "unauthorized" response across this entire app, change that file
       // (see api/responses/unauthorized).
@@ -56,13 +48,10 @@ and exposed as \`req.me\`.)`
       // To customize the response for _only this_ action, replace `responseType` with
       // something else.  For example, you might set `statusCode: 498` and change the
       // implementation below accordingly (see http://sailsjs.com/docs/concepts/controllers).
-    }
-
+    },
   },
 
-
-  fn: async function ({emailAddress, password}) {
-
+  fn: async function ({ emailAddress, password }) {
     // Look up by the email address.
     // (note that we lowercase it to ensure the lookup is always case-insensitive,
     // regardless of which database we're using)
@@ -71,13 +60,14 @@ and exposed as \`req.me\`.)`
     });
 
     // If there was no matching user, respond thru the "badCombo" exit.
-    if(!userRecord) {
+    if (!userRecord) {
       throw 'badCombo';
     }
 
-    // If the password doesn't match, then also exit thru "badCombo".
-    await sails.helpers.passwords.checkPassword(password, userRecord.password)
-    .intercept('incorrect', 'badCombo');
+    // If the password doesn't match, then also exit thru 'badCombo'.
+    await sails.helpers.passwords
+      .checkPassword(password, userRecord.password)
+      .intercept('incorrect', 'badCombo');
 
     // Modify the active session instance.
     // (This will be persisted when the response is sent.)
@@ -88,7 +78,5 @@ and exposed as \`req.me\`.)`
     if (sails.hooks.sockets) {
       await sails.helpers.broadcastSessionChange(this.req);
     }
-
-  }
-
+  },
 };
---
+++ api/controllers/gps/devices.js
@@ -0,0 +1,54 @@
+/**
+ * DevicesController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+const Device = require('../../models/Device');
+
+module.exports = {
+  get: function (req, res) {
+    Device.find()
+      .then((Devices) => {
+        if (!Devices || Devices.lenght === 0) {
+          return res.send({
+            success: false,
+            message: 'No records found',
+          });
+        }
+
+        return res.send({
+          success: true,
+          message: 'Records fetched',
+          data: Devices,
+        });
+      })
+      .catch((err) => {
+        sails.log.debug(err);
+
+        return res.send({
+          success: false,
+          message: 'Unable to getch records',
+        });
+      });
+  },
+
+  create: function (req, res) {
+    sails.log.debug(req.allParams());
+    Device.create(req.allParams())
+      .then((Devices) => {
+        return res.send({
+          success: true,
+          message: 'Record created successfully',
+        });
+      })
+      .catch((err) => {
+        sails.log.debug(err);
+        return res.send({
+          success: false,
+          message: 'Unable to create record',
+        });
+      });
+  },
+};
