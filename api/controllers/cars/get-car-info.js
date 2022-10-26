67bb199b7fe02503a3b268ee6c4e2db13d77071a
Raziel <a01275036@itesm.mx>
2022-10-26 09:45:18 -0500

    feature finished

--- api/controllers/cars/get-car-info.js
+++ api/controllers/cars/get-car-info.js
@@ -8,64 +8,37 @@ module.exports = {
     inputs: {
       id: {
         description: 'The car id".',
-        type: 'string',
+        type: 'number',
         required: true,
       },
     },
   
     exits: {
       success: {
-        description: 'The requesting user agent has been successfully logged in.',
-        extendedDescription: `Under the covers, this stores the id of the logged-in user in the session
-  as the \`userId\` key.  The next time this user agent sends a request, assuming
-  it includes a cookie (like a web browser), Sails will automatically make this
-  user id available as req.session.userId in the corresponding action.  (Also note
-  that, thanks to the included "custom" hook, when a relevant request is received
-  from a logged-in user, that user's entire record from the database will be fetched
-  and exposed as \`req.me\`.)`,
+        description: 'The info has been recived.',
       },
   
       badCombo: {
-        description: `The provided email and password combination does not
-        match any user in the database.`,
-        responseType: 'unauthorized',
-        // ^This uses the custom `unauthorized` response located in `api/responses/unauthorized.js`.
-        // To customize the generic "unauthorized" response across this entire app, change that file
-        // (see api/responses/unauthorized).
-        //
-        // To customize the response for _only this_ action, replace `responseType` with
-        // something else.  For example, you might set `statusCode: 498` and change the
-        // implementation below accordingly (see http://sailsjs.com/docs/concepts/controllers).
+        description: `The provided id does not
+        match any car in the database.`,
+        responseType: '404: not found',
       },
     },
   
-    fn: async function ({ emailAddress, password }) {
+    fn: async function ({ id }) {
       // Look up by the email address.
       // (note that we lowercase it to ensure the lookup is always case-insensitive,
       // regardless of which database we're using)
-      var userRecord = await User.findOne({
-        emailAddress: emailAddress.toLowerCase(),
-      });
+      const carRecord = await Car.findOne(id);
   
       // If there was no matching user, respond thru the "badCombo" exit.
-      if (!userRecord) {
+      if (!carRecord) {
         throw 'badCombo';
       }
   
-      // If the password doesn't match, then also exit thru 'badCombo'.
-      await sails.helpers.passwords
-        .checkPassword(password, userRecord.password)
-        .intercept('incorrect', 'badCombo');
-  
-      // Modify the active session instance.
-      // (This will be persisted when the response is sent.)
-      this.req.session.userId = userRecord.id;
-  
       // In case there was an existing session (e.g. if we allow users to go to the login page
       // when they're already logged in), broadcast a message that we can display in other open tabs.
-      if (sails.hooks.sockets) {
-        await sails.helpers.broadcastSessionChange(this.req);
-      }
+      return carRecord;
     },
   };
   
